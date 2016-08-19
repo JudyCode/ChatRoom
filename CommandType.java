4fc83ca581c2384455210e2743aabecedb7cefb5
Zixuan Lu <lzx0305@gmail.com>
2016-08-18 20:07:58 -0700

    Helper function

---
+++ CommandType.java
@@ -0,0 +1,23 @@
+public class CommandType {
+
+  //used for store the command type
+
+    public static final String AUTHENTICATE = "authenticate";
+    public static final String MESSAGE = "message";
+    public static final String ONLINE = "online";
+    public static final String BLOCK = "block";
+    public static final String UNBLOCK = "unblock";
+    public static final String BROADCAST = "broadcast";
+    public static final String LOGOUT = "logout";
+    public static final String LOGIN_BROADCAST = "loginBroadcast";
+    public static final String LOGOUT_BROADCAST = "logoutBroadcast";
+    public static final String OFFLINE_MESSAGE = "offlineMessage";
+    public static final String HEARTBEAT = "hearbeat";
+    public static final String SERVER_UNAVAILABLE = "serverUnavailable";
+    public static final String GET_ADDRESS = "getaddress";
+    public static final String PRIVATE = "private";
+    public static final String P2P_REQUEST = "p2pRequest";
+
+    public static final String LOGOUT_MULTIPLE_LOGIN = "logout due to multiple login";
+
+}
---
+++ Constants.java
@@ -0,0 +1,20 @@
+public class Constants {
+
+    public static final int LOGIN_BLOCK_TIME_IN_SECONDS = 60;
+    public static final int LOGIN_MAX_FAILED_ATTEMPTS = 3;
+
+    public static final int HEARTBEAT_TIME_IN_SECONDS = 30;
+    public static final int TIME_OUT_IN_SECONDS = 45;
+    public static final int TIME_OUT_CHECK_FREQUENCY_IN_SECONDS = 10;
+
+    public static final int TERMINAL_INPUT_TIMEOUT_IN_SECONDS = 15;
+
+    public static final String BLOCKED = "blocked";
+    public static final String NO_SUCH_USER = "noSuchUser";
+    public static final String OK = "ok";
+    public static final String UNKNOWN = "unknown";
+    public static final String OFFLINE = "offline";
+    public static final String AGREE = "agree";
+    public static final String DISAGREE = "disagree";
+
+}
